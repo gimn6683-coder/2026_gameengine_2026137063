40d16a97bd0f5ea6fe393bf378e1ca6ab6f40de3
gimn6683 <gimn6683@gmail.com>
2026-04-16 16:06:41 +0900

    적, 레벨2 등등

---
+++ 1B_nahyeon/Assets/Settings/LevelObject.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelObject : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+   public string nextLevel;
+    public void MoveToNextLevel()
+    {
+        SceneManager.LoadScene(nextLevel);
+    }
+}
---
+++ 1B_nahyeon/Assets/Settings/playerController.cs
@@ -0,0 +1,42 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class playerController : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Respawn"))
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        }
+
+        if (collision.CompareTag("Finish"))
+        {
+            collision.GetComponent<LevelObject>().MoveToNextLevel();
+        }
+
+        if (collision.CompareTag("Enemy"))
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        }
+        
+
+
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+}
